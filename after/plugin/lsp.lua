55816e587a0cd9b41cc9200da2128b21984fff76
AGomezCuervo <107006627+AGomezCuervo@users.noreply.github.com>
2023-08-11 00:02:47 -0500

    Backup for the lsp and cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,12 +16,19 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
+local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-p>'] = cmp.mapping(function()
+    if cmp.visible() then
+      cmp.select_prev_item(cmp_select_opts)
+    else
+      cmp.complete()
+    end
+  end),
 })
 
 cmp_mappings['<Tab>'] = nil
